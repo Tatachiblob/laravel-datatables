7d8253e758701f03585e64a0f2e721f6a718eba3
Tatachiblob <yuta_inoue@dlsu.edu.ph>
2020-12-25 11:29:19 +0900

    fix migration
    [modify] create_users_table change int to integer
    [add] add session table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('gender');
-            $table->int('age')->default(0);
+            $table->integer('age')->default(0);
             $table->string('instroduction');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
